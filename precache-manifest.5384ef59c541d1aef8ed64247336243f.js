eeec708c53f4c6fb1a9302121049ae049a32087b
Raul Barriga <barrigaraulaug@gmail.com>
2021-10-23 14:43:46 -0700

    Updates

---
+++ precache-manifest.5384ef59c541d1aef8ed64247336243f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45e8b110f3957784bbfc998e356177e",
+    "url": "/Headphones-Page/index.html"
+  },
+  {
+    "revision": "1a578d7f62e9565dc81f",
+    "url": "/Headphones-Page/static/css/main.c77f01ea.chunk.css"
+  },
+  {
+    "revision": "362329218f9756f2525d",
+    "url": "/Headphones-Page/static/js/2.39e7a94b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Headphones-Page/static/js/2.39e7a94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a578d7f62e9565dc81f",
+    "url": "/Headphones-Page/static/js/main.25475713.chunk.js"
+  },
+  {
+    "revision": "e8b56903fd33daaf2a50",
+    "url": "/Headphones-Page/static/js/runtime-main.095b9020.js"
+  },
+  {
+    "revision": "b7bc026b19376ce4fbc8c93b3a4e51ee",
+    "url": "/Headphones-Page/static/media/Daco_4871547.b7bc026b.png"
+  },
+  {
+    "revision": "ad93b255e78fba7030ef006a2d310315",
+    "url": "/Headphones-Page/static/media/large-headphone.ad93b255.ad93b255.png"
+  },
+  {
+    "revision": "c4ec6822047ebe3e7b8a3e66ebd6d97e",
+    "url": "/Headphones-Page/static/media/pink-headphones.c4ec6822.c4ec6822.png"
+  },
+  {
+    "revision": "de0533f5415e07620bdba044b8a5177a",
+    "url": "/Headphones-Page/static/media/yellow-headphones.de0533f5.de0533f5.png"
+  }
+]);
